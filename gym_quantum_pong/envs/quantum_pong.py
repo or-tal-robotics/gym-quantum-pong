7cfa2b0a731ba28539e31dec5d34ff3560e94372
ortslil64 <ortslil64@gmail.com>
2019-11-11 12:29:36 +0200

    dfsa

--- gym_quantum_pong/envs/quantum_pong.py
+++ gym_quantum_pong/envs/quantum_pong.py
@@ -3,7 +3,7 @@ import time
 import cv2
 
 class Player():
-    def __init__(self, x, board_size, bat_size, dtheta = 15, dy = 3):
+    def __init__(self, x, board_size, bat_size, dtheta = np.pi/12, dy = 3):
         self.x = x
         self.y = np.random.randint(bat_size+1, board_size[0] - bat_size-2)
         self.theta = np.random.uniform(0,2*np.pi)
@@ -35,17 +35,19 @@ class Ball():
         self.y = y
         self.theta = theta
         self.quantum_hits = 0
+        self.polarization = np.random.uniform(0.0, np.pi)
+        self.visible = 255
     
 
 
 
 class QuantumPong():
-    def __init__(self, n_players = 1, board_size = (60,60,50), V = 2, n_rounds = 21, res = 0.2, mode="quantum"):
+    def __init__(self, n_players = 1, board_size = (60,60,60), V = 2, n_rounds = 21, res = 0.2, mode="quantum"):
         self.bat_size = 6
         self.board_size = board_size
         self.board = np.zeros((int(board_size[0]/res),int(board_size[1]/res)))
         self.res = res
-        self.ball = Ball(x = board_size[0]/2, y = board_size[1]/2, V = V, theta = np.random.uniform(0, 2*np.pi))
+        self.ball = Ball(x = board_size[0]/2, y = board_size[1]/2, V = V, theta = np.random.uniform(np.pi/6, np.pi/2 - np.pi/6))
         self.mode = mode
         self.left_player = Player(6, board_size, self.bat_size)
         self.right_player = Player(board_size[1] - 6, board_size, self.bat_size)
@@ -56,41 +58,27 @@ class QuantumPong():
         self.board_angle = np.arctan((board_size[0]-board_size[2])/board_size[1])
         self.quantum_hits = 0
         
-    def quantum_correlation(self, pose):
-        if pose == 'left':
-            t1 = np.remainder(self.left_player.theta,2*np.pi)
-            t1 = np.min([t1, 2*np.pi-t1])
-            t2 = np.remainder(self.right_player.theta_mesured,2*np.pi)
-            t2 = np.min([t2, 2*np.pi-t2])
-            x = np.abs(t1-t2)
-            P = (1 + np.cos(x))/2
-        elif pose == 'right':
-            t1 = np.remainder(self.left_player.theta_mesured,2*np.pi)
-            t1 = np.min([t1, 2*np.pi-t1])
-            t2 = np.remainder(self.right_player.theta,2*np.pi)
-            t2 = np.min([t2, 2*np.pi-t2])
-            x = np.abs(t1-t2)
-            P = (1 + np.cos(x))/2
-            
-        return P
+
     
-    def classical_correlation(self, pose):
+    def direction_probability(self, pose):
         if pose == 'left':
             t1 = np.remainder(self.left_player.theta,2*np.pi)
             t1 = np.min([t1, 2*np.pi-t1])
-            t2 = np.remainder(self.right_player.theta_mesured,2*np.pi)
+            t2 = np.remainder(self.ball.polarization,2*np.pi)
             t2 = np.min([t2, 2*np.pi-t2])
             x = np.abs(t1-t2)
-            P = 1 - x/np.pi
+            P = (1 + np.cos(2*x))/2
         elif pose == 'right':
-            t1 = np.remainder(self.left_player.theta_mesured,2*np.pi)
+            t1 = np.remainder(self.ball.polarization,2*np.pi)
             t1 = np.min([t1, 2*np.pi-t1])
             t2 = np.remainder(self.right_player.theta,2*np.pi)
             t2 = np.min([t2, 2*np.pi-t2])
             x = np.abs(t1-t2)
-            P = 1 - x/np.pi
+            P = (1 + np.cos(2*x))/2
             
         return P
+
+    
         
 
     
@@ -108,9 +96,9 @@ class QuantumPong():
         self.board = np.zeros((round(self.board_size[1]/self.res),round(self.board_size[0]/self.res)))
         Bx = int(round(self.ball.x/self.res))
         By = int(round(self.ball.y/self.res))
-        cv2.circle(self.board,(By, Bx), 10, (255,255,255), -1)
+        cv2.circle(self.board,(By, Bx), 10, (self.ball.visible,self.ball.visible,self.ball.visible), -1)
         
-        self.board[50:-50,50:-50] = 0
+        #self.board[50:-50,50:-50] = 0
 
         
         for ii in range(-round(self.bat_size/self.res),round(self.bat_size/self.res)+1):
@@ -123,13 +111,13 @@ class QuantumPong():
             self.board[ii,0:3] = 200
          
         
-        x_tor = np.array([round(self.left_player.x/self.res), round(self.left_player.x/self.res) + (5/self.res)*np.sin(np.deg2rad(self.left_player.theta))]).astype(np.int)
-        y_tor = np.array([round(self.left_player.y/self.res), round(self.left_player.y/self.res) + (5/self.res)*np.cos(np.deg2rad(self.left_player.theta))]) .astype(np.int)
+        x_tor = np.array([round(self.left_player.x/self.res), round(self.left_player.x/self.res) + (5/self.res)*np.sin((self.left_player.theta))]).astype(np.int)
+        y_tor = np.array([round(self.left_player.y/self.res), round(self.left_player.y/self.res) + (5/self.res)*np.cos((self.left_player.theta))]) .astype(np.int)
         self.board = cv2.line(self.board,(y_tor[0],x_tor[0] ),(y_tor[1],x_tor[1] ),255,3)
         
        
-        x_tor = np.array([round(self.right_player.x/self.res), round(self.right_player.x/self.res) + (5/self.res)*np.sin(np.deg2rad(self.right_player.theta))]).astype(np.int)
-        y_tor = np.array([round(self.right_player.y/self.res), round(self.right_player.y/self.res) + (5/self.res)*np.cos(np.deg2rad(self.right_player.theta))]) .astype(np.int)
+        x_tor = np.array([round(self.right_player.x/self.res), round(self.right_player.x/self.res) + (5/self.res)*np.sin((self.right_player.theta))]).astype(np.int)
+        y_tor = np.array([round(self.right_player.y/self.res), round(self.right_player.y/self.res) + (5/self.res)*np.cos((self.right_player.theta))]) .astype(np.int)
         self.board = cv2.line(self.board,(y_tor[0],x_tor[0] ),(y_tor[1],x_tor[1] ),255,3)
         
 #        
@@ -149,8 +137,12 @@ class QuantumPong():
         # --- Ball step --- #
         self.ball.x +=  self.ball.V * np.cos(self.ball.theta)
         self.ball.y +=  self.ball.V * np.sin(self.ball.theta)
+        self.ball.visible -= 20 
             
         # --- Ball --- #
+        if self.ball.visible < 50:
+            self.ball.visible = 50
+            
         if self.ball.y > self._height(self.ball.x):
             self.ball.y = self._height(self.ball.x)
             self.ball.theta = - self.ball.theta - 2*self.board_angle
@@ -167,72 +159,53 @@ class QuantumPong():
         if self.ball.y >= self.left_player.y - self.bat_size and self.ball.y <= self.left_player.y + self.bat_size and self.ball.x <= self.left_player.x :
             self.ball.x = self.left_player.x
             hit = 1
-            if self.quantum_hits == 0:
-                if np.random.binomial(1,0.5) == 1:
-                    self.ball.theta = np.pi - self.ball.theta
-                else:
-                    self.ball.theta = np.pi + self.ball.theta
-                self.ball.theta += np.random.normal(0,0.1)
-                    
-                self.ball_theta_last = self.ball.theta
-                self.left_player.theta_mesured = self.left_player.theta
-                self.quantum_hits += 1
-            elif self.quantum_hits == 1:
-                if self.mode == "quantum":
-                    p = self.quantum_correlation('left')
-                elif self.mode == "classic":
-                    p = self.classical_correlation('left')
-                else:
-                    print("Error with mode setting!")
-                if np.random.binomial(1,p) == 1:
-                    self.ball.theta = np.pi - self.ball.theta
-                else:
-                    self.ball.theta = np.pi + self.ball.theta
-                self.quantum_hits = 0
+            p = self.direction_probability("left")
+            if np.random.binomial(1,p) == 1:
+                self.ball.theta = np.pi - self.ball.theta
+                self.ball.polarization = self.left_player.theta
+            else:
+                self.ball.theta = np.pi + self.ball.theta
+                self.ball.polarization = self.left_player.theta + np.pi/2
                 
-        elif self.ball.x <= self.left_player.x:
+        
+                
+        elif self.ball.x <= self.left_player.x and self.ball.x > self.left_player.x - 3:
             self.right_player.score += 1
-            self.ball.x = self.board_size[1]/2
-            self.ball.y = self.board_size[0]/2
-            self.ball.theta = np.random.uniform(0, 2*np.pi)
             self.round += 1
             win = -1
+            self.ball.visible = 255
+            
+        elif self.ball.x <= self.left_player.x - 3:
+            self.ball.x = self.board_size[1]/2
+            self.ball.y = self.board_size[0]/2
+            self.ball.theta = np.random.uniform(np.pi/6, np.pi/2 - np.pi/6)
+            self.ball.visible = 255
+        
+    
                 
         if self.ball.y >= self.right_player.y - self.bat_size and self.ball.y <= self.right_player.y + self.bat_size and self.ball.x >= self.right_player.x :
             self.ball.x = self.right_player.x
             hit = -1
-            if self.quantum_hits == 0:
-                if np.random.binomial(1,0.5) == 1:
-                    self.ball.theta = np.pi - self.ball.theta
-                else:
-                    self.ball.theta = np.pi + self.ball.theta
-                self.ball.theta += np.random.normal(0,0.1)
-                
-                self.ball_theta_last = self.ball.theta
-                self.right_player.theta_mesured = self.right_player.theta
-                self.quantum_hits += 1
-                
-            elif self.quantum_hits == 1:
-                if self.mode == "quantum":
-                    p = self.quantum_correlation('right')
-                elif self.mode == "classic":
-                    p = self.classical_correlation('right')
-                else:
-                    print("Error with mode setting!")
-                if np.random.binomial(1,p) == 1:
-                    self.ball.theta = np.pi - self.ball.theta
-                else:
-                    self.ball.theta = np.pi + self.ball.theta
-                self.quantum_hits = 0
+            p = self.direction_probability("right")
+            if np.random.binomial(1,p) == 1:
+                self.ball.theta = np.pi - self.ball.theta
+                self.ball.polarization = self.right_player.theta
+            else:
+                self.ball.theta = np.pi + self.ball.theta
+                self.ball.polarization = self.right_player.theta + np.pi/2
                 
            
-        elif self.ball.x >= self.right_player.x:
+        elif self.ball.x >= self.right_player.x and self.ball.x < self.right_player.x + 3:
             self.left_player.score += 1
-            self.ball.x = self.board_size[1]/2
-            self.ball.y = self.board_size[0]/2
-            self.ball.theta = np.random.uniform(0, 2*np.pi)
             self.round += 1
             win = 1
+            self.ball.visible = 255
+            
+        elif self.ball.x >= self.right_player.x + 3:
+            self.ball.x = self.board_size[1]/2
+            self.ball.y = self.board_size[0]/2
+            self.ball.theta = np.random.uniform(np.pi/6, np.pi/2 - np.pi/6)
+            self.ball.visible = 255
                 
         
         if self.round == 21:
@@ -252,7 +225,7 @@ if __name__ == '__main__':
     while not done:
         a = QP.step(3,4)
         done = a[2]
-        cv2.imshow("video",a[1])
+        cv2.imshow("video",a[1]/255)
         time.sleep(0.1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
