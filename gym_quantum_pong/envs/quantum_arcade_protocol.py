abd2d8922c576b1a38f43795194e19344ca42c9f
ortslil64 <ortslil64@gmail.com>
2019-12-06 13:20:25 +0200

    Create quantum_arcade_protocol.py

---
+++ gym_quantum_pong/envs/quantum_arcade_protocol.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# experiments protocols:
+1. [c00 + c01 + c10 - c11], Where the left player have full controll over the entaglement (theta_ent)
+2. [c00 + c01 + c10 - c11], Where the left player have no controll over the entaglement (theta_ent = pi/4)
+3. [c00 + c01 + c10 - c11], Where the left player have no controll over the entaglement (theta_ent = pi/6)
+4. [c00 + c01 + c10 - c11], Where the left player have no controll over the entaglement (theta_ent = 0)
+
+1. [c00 + c01 + c10 + c11], Where the left player have full controll over the entaglement (theta_ent)
+2. [c00 + c01 + c10 + c11], Where the left player have no controll over the entaglement (theta_ent = pi/4)
+3. [c00 + c01 + c10 + c11], Where the left player have no controll over the entaglement (theta_ent = pi/6)
+4. [c00 + c01 + c10 + c11], Where the left player have no controll over the entaglement (theta_ent = 0)
+
